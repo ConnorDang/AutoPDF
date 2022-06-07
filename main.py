900f32521d8a3ffd95d71c07b7ab13eebe0e1a4d
Connor Dang <c.dang@berkeley.edu>
2022-06-06 23:08:42 -0700

    PDF Write Template Complete

--- main.py
+++ main.py
@@ -1,95 +1,70 @@
 ######## IMPORTS #########
-import sys
-import os
+from tarfile import GNUTYPE_LONGNAME
+import PyPDF2
 import time
-#from io import StringIO
-
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import letter
 
-"""
-####### VARIABLES ########
-# Get the filename and extension so we can use it for renaming the newly-created file
-filename, file_extension = os.path.splitext(sys.argv[1])
 
-# Append "-filled" to the filename and save it in the same place
-# This retains the original file so it can be used again with the script
-# It also saves the file in the same folder so it's easy to find
-filled_out_file = filename + "-filled" + file_extension
-
-# Re-use for any checkboxes
-checkbox = "X"
-
-# Information for the form
-# Varibles and their names can be changed to anything depending on the form being filled out
-# Date of service and date needed by can be a second argument since the rest of the information will stay pretty much the same
-dates_of_service = sys.argv[2]
-date_needed_by = sys.argv[3]
-patient_name = "Hugh"
-patient_dob = "01/01/1970"
-hospital_name = "Sickbay"
-hospital_address = "709 Starfleet Way"
-hospital_phone = "123.456.7890"
-release_to = "Jacob Salmela"
-phone_number = "123.456.7890"
-address = "Deck 10, Enterprise D"
-date_signed = time.strftime("%d/%m/%Y")
-
-####### FUNCTIONS ########
-def main():
-    packet = StringIO.StringIO()
-    # Create a new PDF with Reportlab
-    can = canvas.Canvas(packet, pagesize=letter)
-
-    # This would do better in a loop using some key/value pairs, but this is good enough for government work
-    can.drawString(221, 705, patient_name)
-    can.drawString(510, 705, patient_dob)
-    can.drawString(168, 673, hospital_name)
-    can.drawString(168, 650, hospital_address)
-    can.drawString(448, 648, hospital_phone)
-    can.drawString(237, 608, release_to)
-    can.drawString(451, 588, phone_number)
-    can.drawString(176, 585, address)
-    can.drawString(488, 552, checkbox) # Purpose of release
-    can.drawString(301, 508, date_needed_by)
-    can.drawString(288, 385, checkbox) # Release method
-    can.drawString(299, 480, dates_of_service)
-    can.drawString(215, 480, checkbox) # Dates of service checkbox
-    can.drawString(163, 406, checkbox) # All health information
-    can.drawString(449, 140, date_signed)
-    can.drawString(302, 99, checkbox) # Relationship
-
-    # Apply the changes
-    can.save()
-
-    # Move to the beginning of the StringIO buffer
-    packet.seek(0)
-    new_pdf = PdfFileReader(packet)
-
-    # Read the existing PDF (the first argument passed to this script)
-    existing_pdf = PdfFileReader(file(filename + file_extension, "rb"))
-    output = PdfFileWriter()
-
-    # Add the "watermark" (which is the new pdf) on the existing page
-    page = existing_pdf.getPage(0)
-    page.mergePage(new_pdf.getPage(0))
-    output.addPage(page)
-
-    # Finally, write "output" to a real file
-    outputStream = file(filled_out_file, "wb")
-    output.write(outputStream)
-    outputStream.close()
-"""
+# Import in GIMP as 1.000 pixels/pt - 612 x 792 pts
+# GIMP uses bottom-left origin. Canvas uses top-left as origin.
+# Helper function to compute Canvas Y
+def trueY (GIMP_Y) :
+    return 792 - GIMP_Y
 
 def main():
-    print("Imports Successful")
+
+    ### VARIABLES
+    GUY = "Sean Dang"
+
     pdf = canvas.Canvas("temp.pdf", pagesize = letter)
-    pdf.drawString(215, 480, "X")
-    pdf.drawString(163, 406, "X")
+    # Header Section
+    pdf.drawString(39, trueY(95), "534")
+    pdf.drawString(196, trueY(95), "12")
+    pdf.drawString(273, trueY(95), "12/05/2020")
+    pdf.drawString(420, trueY(95), "X")
+    pdf.drawString(35, trueY(130), "350119")
+    pdf.drawString(190, trueY(130), "Menoa Aghajani")
+    pdf.drawString(423, trueY(130), "FPEA II")
+
+    # Mid Section
+    pdf.drawString(450, trueY(236), GUY)
+    pdf.drawString(116, trueY(267), "9135 Tampa Avenue")
+    pdf.drawString(495, trueY(267), "F20-01526")
+    pdf.drawString(80, trueY(310), "1000 HOURS ON 12/05/20")
+    pdf.drawString(375, trueY(310), "1200 HOURS ON 12/05/20")
+    pdf.drawString(125, trueY(355), "2.0")
+
     pdf.save()
+    print("Layer Created.")
+    time.sleep(2)
 
+    # Add as watermark.
+    blankFile = "blank.pdf"
+    watermark = "temp.pdf"
+    mergeFile = "merged.pdf"
 
+    input_file = open(blankFile,'rb')
+    input_pdf = PyPDF2.PdfReader(input_file)
 
+    watermark_file = open(watermark,'rb')
+    watermark_pdf = PyPDF2.PdfReader(watermark_file)
+
+    pdf_page = input_pdf.pages[0]
+    watermark_page = watermark_pdf.pages[0]
+
+    pdf_page.merge_page(watermark_page)
+
+    output = PyPDF2.PdfWriter()
+    output.add_page(pdf_page)
+    merged_file = open(mergeFile,'wb')
+    output.write(merged_file)
+
+    merged_file.close()
+    watermark_file.close()
+    input_file.close()
+
+    print("Layer Written Successfully.")
 
 
 ######### SCRIPT #########
